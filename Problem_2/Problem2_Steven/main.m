24840fe158c7b883eff1561024bd56f659ce5435
npotdar <nikhil.potdar94@hotmail.com>
2016-03-30 22:05:38 +0200

    Comments and fixes

--- Problem_2/Problem2_Steven/main.m
+++ Problem_2/Problem2_Steven/main.m
@@ -7,16 +7,21 @@
 % @date:    28/03/2016    
 % @subject: KBCS SC4081 MATLAB
 
+
+% Include fis files for Fuzzy Controller
 proportional = readfis('proportional.fis');
 derivative = readfis('derivative.fis');
 
 % Fuzzy supervisory control ruleviewer will open but wait for results
 disp('Running Fuzzy Supervisory Controller ...');
+
+% Obtain simulation ouputs for plotting
 simout = sim('Sim_model.mdl');
 t = out(:,1);
 insig = out(:,2);
 modelout = out(:,3);
 
+% Plotting
 figure;
 hold on;
 plot(t,insig);
--- neural_network/nn_solver_main.m
+++ neural_network/nn_solver_main.m
@@ -10,23 +10,30 @@
 %
 
 clear all; close all; clc; 
+set(0,'DefaultAxesFontSize',12)
 
-%% 
-% # USER CONFIGURATION
+%%     
+% NEURAL NETWORK DEFNIITION
 
-% Mode 
-ota_p = false;               % true: One-step ahead predicition / false: simulation
-    
-% Neural Network
-inputDelays = 1:2;          % Number of delays to use / 1:2 means x(t-1) and x(t-2) is used to estimate y(t) for series-parallel NARX
-feedbackDelays = 1:2;       % Number of feedback delays to use / 1:2 means y(t-1) and y(t-2) is used to estimate y(t) for series-parallel NARX
-hiddenLayersize = 1;        % Number of NN hidden layers
+% Number of delays to use / 1:2 means x(t-1) and x(t-2) is used to 
+% estimate y(t) for series-parallel NARX
+inputDelays = 1:3; 
 
-% Data set 
-trainfactor = 0.8;          % Percentage of id data set for training, remaining for MATLAB validation of overfitting
+% Number of feedback delays to use / 1:2 means y(t-1) and y(t-2)
+% is used to estimate y(t) for series-parallel NARX
+feedbackDelays = 1:3; 
 
-% S/W Performance Boost
-USE_PARALLEL = 'no';        % 'yes': Possibly faster for higher number of layers/delays / 'no': disable
+% Number of NN hidden layers
+hiddenLayersize = 3;        
+
+% DATA SET 
+% Percentage of id data set for training, remaining for 
+% MATLAB validation of overfitting
+trainfactor = 0.8;         
+
+% S/W PERFORMANCE BOOST
+% 'yes': Possibly faster for higher number of layers/delays / 'no': disable
+USE_PARALLEL = 'no';        
 
 
 %% DO NOT EDIT BELOW THIS LINE 
@@ -36,44 +43,32 @@ disp(' ');
 disp(' Neural Network Dynamic Time Series Analysis');
 disp(' ');
 
-
+% Load external identification and validation data
 load iddata-05.mat
 
-% # Prepare data for neural network toolbox
+% PREPARE DATA FOR NN TOOLBOX
 
 % Training data set
-
 id_u = iddata.u;
-id_y = (iddata.y); %+(rand(numel(iddata.y),1));
+id_y = (iddata.y);
 id_ts = iddata.Ts;
 
-% Validation data set
-
-val_u = valdata.u; %.*(rand(numel(valdata.u),1).*5) %NOISE;
-val_y = valdata.y;
-val_ts = valdata.Ts;
+% Testing data set (validation data set)
+test_u = valdata.u;
+test_y = valdata.y;
+test_ts = valdata.Ts;
 
 % Combine data set and use index splitting later
-
-inputData = [id_u; val_u].';
-outputData = [id_y; val_y].';
+inputData = [id_u; test_u].';
+outputData = [id_y; test_y].';
 
 %% 
-% # Create NARX neural network 
+% CREATE NARX NEURAL NETWORK
 % Uses input and output history values for dynamic system. This create a 
 % series-parallel network which uses value of the input x and provided 
 % output values upto specified delays to predict the NN output
 
 net = narxnet(inputDelays,feedbackDelays,hiddenLayersize);
-
-modifier = 0;
-if ota_p
-    net = removedelay(net);         % Sets to one-step ahead predicition 
-    net.name = [net.name ' - Predict One Step Ahead'];
-    modifier = 1;
-end
-
-%net = init(net);            % Initialises the neural net
 
 % Set network sample time
 
@@ -81,64 +76,107 @@ net.sampleTime = id_ts;
 
 % Preparation of the data for the neural network
 
-[data_in, data_instate, data_layerstate, data_out, ~, data_shift] = preparets(net,num2cell(inputData),{},num2cell(outputData));
+[data_in, data_instate, data_layerstate, data_out, ~, data_shift] = ...
+    preparets(net,num2cell(inputData),{},num2cell(outputData));
 
-% # Divide the data into training and testing/validation, the test data is
+% Divide the data into training, MATLAB validation, and testing. The test data is
 % only used for testing the neural network's performance
 
 % Determine indices 
 
 ind_train_up = numel(id_u)*trainfactor;		% Training data upper bound index
-ind_val_low = ind_train_up+1;					% MATLAB Validation data lower bound index
+ind_val_low = ind_train_up+1;				% MATLAB Validation data lower bound index
 ind_val_up = numel(id_u);					% MATLAB Validation data upper bound index
-ind_test_low = ind_val_up+1;					% Testing data lower bound index
-ind_test_up = ind_val_up + numel(val_u);		% Testing data upper bound index
+ind_test_low = ind_val_up+1;				% Testing data lower bound index
+ind_test_up = ind_val_up + numel(test_u);	% Testing data upper bound index
 
 % Divide data for NN by index
 net.divideFcn = 'divideind'; 
 
-[net.divideParam.trainInd, net.divideParam.valInd, net.divideParam.testInd] = divideind(numel(outputData) , 1:ind_train_up , ind_val_low:ind_val_up , ind_test_low:ind_test_up );
+[net.divideParam.trainInd, net.divideParam.valInd, net.divideParam.testInd] ...
+    = divideind(numel(outputData) , 1:ind_train_up , ...
+    ind_val_low:ind_val_up , ind_test_low:ind_test_up );
 
 %% 
 % # Network training
 % returns trained network and training including number of iterations 
 % and performance MSE
 
-[net, trainrecord] = train(net, data_in, data_out, data_instate, data_layerstate,'useParallel',USE_PARALLEL);
+[net, trainrecord] = train(net, data_in, data_out, data_instate, ...
+    data_layerstate,'useParallel',USE_PARALLEL);
+
+% Save trained network to other variable
+
+net_org = net;
 
 %%
-% # Test the network and performance calculations
-% fixme: CHECK THE INDICES FOR RMS CALCULATIONS
+% # Simulation and One step ahead Network
 
-% Preparation of the data for the neural network testing
-[val_in, val_instate, val_layerstate, val_out, ~, val_shift] = preparets(net,num2cell(val_u.'),{},num2cell(val_y.'));
+for i = 1:2
+    
+% Create simulation then one-step ahead rms and plots
+if i==2
+    disp('One Step Ahead');
+    net = removedelay(net_org);         % Sets to one-step ahead predicition 
+    net.name = [net.name ' - Predict One Step Ahead'];
+    modifier = 1;
+else
+    disp('Simulation');
+    net = closeloop(net_org);
+    net.name = [net.name ' - Simulation'];  
+    modifier = 0;
+end
 
-test_out = net(val_in, val_instate, val_layerstate);
-test_performance = perform(net, val_out, test_out);
 
-% Performance calculations 
-test_rms = rms(cell2mat(gsubtract(val_out, test_out)));
+% Preparation of the data for neural network train data and NN 
+[train_in, train_instate, train_layerstate, train_out, ~, train_shift] = ...
+    preparets(net,num2cell(id_u.'),{},num2cell(id_y.'));
+train_net_out = net(train_in, train_instate, train_layerstate);
+train_rms = rms(cell2mat(gsubtract(train_out(1:(end-modifier)), ...
+    train_net_out(1:(end-modifier)))));
+
+disp(['Root-mean square error of train data set: ', num2str(train_rms)]);
+
+% Preparation of the data for the neural network testing and NN
+[test_in, test_instate, test_layerstate, test_out, ~, test_shift] = ...
+    preparets(net,num2cell(test_u.'),{},num2cell(test_y.'));
+test_net_out = net(test_in, test_instate, test_layerstate);
+test_rms = rms(cell2mat(gsubtract(test_out(1:(end-modifier)), ...
+    test_net_out(1:(end-modifier)))));
 
 disp(['Root-mean square error of test data set: ', num2str(test_rms)]);
 
-% Plotting time-series response
+% PLOTTING TIME RESPONSE
+% Moving time-series based on number of Delays included 
 
-% figure, plotresponse(targets,outputs)
+train_time_series = (train_shift:(numel(train_out)+train_shift-1))*id_ts;    
+test_time_series = (test_shift:(numel(test_out)+test_shift-1))*test_ts;    
 
-val_out_plt = cell2mat(val_out);
-test_out_plt = cell2mat(test_out);
+% Training and neural network output plot
 
-time_series = (val_shift:(numel(val_out)+val_shift-1))*val_ts;    % Moving time-series based on number of Delays included as data set is shifted for initiation 
-
-
-figure(1);
-p1 = plot(time_series, val_out_plt,'--', time_series, test_out_plt);
+fig1 = figure(1);
+p1 = plot(train_time_series, cell2mat(train_out), ':', ...
+    train_time_series, cell2mat(train_net_out));
 p1(1).LineWidth = 1.5; 
-title('Time-series Response of Test and Neural Network output');
 ylabel('Output [-]');
 xlabel('Time [s]');
-legend('Test data','Neural Network output');
+legend('Train data','Neural Network output', 'location', 'southwest');
 grid on
 
+print(fig1,strcat('fig_train',num2str(i)),'-depsc');
 
- 
+% Testing and neural network output plot
+
+fig2 = figure(2);
+p1 = plot(test_time_series, cell2mat(test_out), ':', ...
+    test_time_series, cell2mat(test_net_out));
+p1(1).LineWidth = 1.5; 
+ylabel('Output [-]');
+xlabel('Time [s]');
+legend('Test data','Neural Network output','location','southeast');
+grid on
+
+print(fig2,strcat('fig_test',num2str(i)),'-depsc');
+
+pause
+end
