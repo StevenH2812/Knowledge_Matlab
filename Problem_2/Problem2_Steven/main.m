99755cba0210f204a72896eb592ee8fb05847d12
npotdar <nikhil.potdar94@hotmail.com>
2016-04-01 11:41:34 +0200

    Changes

--- Problem_2/Problem2_Steven/main.m
+++ Problem_2/Problem2_Steven/main.m
@@ -7,7 +7,7 @@
 % @date:    28/03/2016    
 % @subject: KBCS SC4081 MATLAB
 
-
+set(0,'DefaultAxesFontSize',12)
 % Include fis files for Fuzzy Controller
 proportional = readfis('proportional.fis');
 derivative = readfis('derivative.fis');
@@ -21,12 +21,27 @@ t = out(:,1);
 insig = out(:,2);
 modelout = out(:,3);
 
+% Unsupervised system closed loop results
+simout = sim('Sim_model_nosupervisor.mdl');
+t_ns = simout2.Time;
+insig_ns = simout2.Data(:,1);
+modelout_ns = simout2.Data(:,2);
+
 % Plotting
-figure;
-hold on;
-plot(t,insig);
-plot(t,modelout);
+figure(1)
+plot(t,insig, t, modelout);
 xlabel('Time [s]');
 ylabel('Response [-]');
 legend('Reference','Model Output','location','southeast');
 grid on
+
+print -depsc fuzzy_supervisory
+
+figure(2)
+plot(t_ns,insig_ns, t_ns, modelout_ns);
+xlabel('Time [s]');
+ylabel('Response [-]');
+legend('Reference','Model Output','location','southeast');
+grid on
+
+print -depsc non_supervised
